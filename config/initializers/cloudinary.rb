eb936cda45fe83998feceee7ad045f1be7256896
reinoptland <reinoptland@gmail.com>
2016-08-12 11:34:08 +0200

    Configured cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'speltaculair'
+  config.api_key = '328922945975162'
+  config.api_secret = 'iVJwRaabMZhhShw-bflO784byNQ'
+  config.cdn_subdomain = true
+end
