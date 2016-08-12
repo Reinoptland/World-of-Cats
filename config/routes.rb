975f81ce7e126a65e080fde1380033cf6ebfb836
reinoptland <reinoptland@gmail.com>
2016-08-12 11:14:15 +0200

    route for news_items

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :news_items, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "news_items#index"
+
+  get "news_items" => "news_items#index"
 end
