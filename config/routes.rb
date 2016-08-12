f2da7dc7450dc7dcb8ef2a0cd97825dd9df0d5f2
reinoptland <reinoptland@gmail.com>
2016-08-12 10:14:19 +0200

    scaffold for news items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :news_items, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "news_items#index"
 end
